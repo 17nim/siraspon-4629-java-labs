d40db851e89b31192e395fcc03469c9813e9659a
17nim <69594054+17nim@users.noreply.github.com>
2022-12-08 23:05:10 +0700

    Add files via upload

---
+++ lab2/Footballer.java
@@ -0,0 +1,14 @@
+public class Footballer {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("java Footballer <name> <nationality> <club>") ;
+            System.exit(0) ;
+        }
+        String name = args[0] ;
+        String nationality = args[1] ;
+        String club = args[2] ;
+        System.out.println("My favorite football player is " + name + ".") ;
+        System.out.println("His nationality is " + nationality + ".") ;
+        System.out.println("He plays for " + club + ".") ;
+    }
+}
