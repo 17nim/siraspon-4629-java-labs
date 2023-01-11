5d338cf287c5b090659cabdcb67fde1ad047b4dd
17nim <17nim.m@gmail.com>
2023-01-11 12:57:27 +0700

    Add lab5\TestPolygons.java

--- saengnak/siraspon/lab5/TestPolygons.java
+++ saengnak/siraspon/lab5/TestPolygons.java
@@ -1,7 +1,8 @@
 package saengnak.siraspon.lab5;
 
 enum PolygonType {
-    RECTANGLE, TRIANGLE
+    RECTANGLE,
+    TRIANGLE
 }
 
 class Polygon {
@@ -16,11 +17,42 @@ class Polygon {
                 return width * height;
 
             case TRIANGLE:
-                return width * height;
+                return (width * height) / 2;
+
+            default:
+                return 0;
         }
+    }
+
+    static void setNumPolygons(int numNewPolygons) {
+        numPolygons += numNewPolygons;
+    }
+
+    static void setSumPolygonsArea(double newPolygonsArea) {
+        sumPolygonsArea += newPolygonsArea;
     }
 }
 
 public class TestPolygons {
+    public static void main(String[] args) {
+        Polygon polygon1 = new Polygon();
+        polygon1.width = 2;
+        polygon1.height = 3;
+        polygon1.type = PolygonType.RECTANGLE;
+        Polygon.setNumPolygons(1);
+        Polygon.setSumPolygonsArea(polygon1.getArea());
 
+        Polygon polygon2 = new Polygon();
+        polygon2.width = 3;
+        polygon2.height = 4;
+        polygon2.type = PolygonType.TRIANGLE;
+        Polygon.setNumPolygons(1);
+        Polygon.setSumPolygonsArea(polygon2.getArea());
+
+        System.out.println("The number of polygons is " + Polygon.numPolygons + " or "
+                + polygon1.numPolygons + " or " + polygon2.numPolygons);
+        System.out.println("The width of polygon1 is " + polygon1.width
+                + " the width of polygon2 is " + polygon2.width);
+        System.out.println("The sum of polygons area is " + Polygon.sumPolygonsArea);
+    }
 }
