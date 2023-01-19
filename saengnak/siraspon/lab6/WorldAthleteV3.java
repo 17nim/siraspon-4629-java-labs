1b4cae7ae4c765ee5fafacad9917845dbd554190
17nim <17nim.m@gmail.com>
2023-01-19 23:17:24 +0700

    Add WorldAthleteV3.java (unfinished)

---
+++ saengnak/siraspon/lab6/WorldAthleteV3.java
@@ -0,0 +1,13 @@
+package saengnak.siraspon.lab6;
+
+import saengnak.siraspon.lab5.*;
+
+class AthleteV2 extends saengnak.siraspon.lab5.Athlete {
+    void practice() {
+        System.out.println(name );
+    }
+}
+
+public class WorldAthleteV3 {
+    
+}
